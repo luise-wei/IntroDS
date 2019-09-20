340884980ad052cba7481186c55d37e362dc8cfe
luise-wei <lu.weickhmann@gmx.de>
2019-09-20 18:29:11 +0300

    added a in-memory db instead of a local db (thereby we have both options for later use) please comment the db.serialize() functions after one run, otherwise your in-memory db will fill up with the same given data every time.

--- northern-lights-app-backend/routes/sqliteAPI.js
+++ northern-lights-app-backend/routes/sqliteAPI.js
@@ -2,25 +2,38 @@ const sqlite3 = require('sqlite3').verbose();
 
 var router = require('express').Router();
 
-var db = new sqlite3.Database('C:/Users/Luise/sqlite3/test.db');
+var db = new sqlite3.Database('./test.db');
 
 console.log(db);
 
-// db.serialize(function() {  
-//     db.run("CREATE TABLE IF NOT EXISTS NodeTest (Number INTEGER)");  
-
-//     var stmt = db.prepare("INSERT INTO NodeTest VALUES (?)");
-//     for (var i = 0; i < 10; i++) {
-//         stmt.run("node " + i);
-//     }
-//     stmt.finalize();
-// }); 
-
-db.serialize(() => {
-    var result = db.run('SELECT * FROM Weather;');
-    console.log(result);
+db.serialize(()=>{
+    db.run("CREATE TABLE IF NOT EXISTS Weather (Temperature FLOAT(4,2), Date DATE, Location varchar(255))");  
 })
 
+db.serialize(() => {  
+    location = 'Helsinki';
+    dates = ['2019-09-01', '2019-09-03','2019-09-05', '2019-09-07',
+             '2019-09-09', '2019-09-11', '2019-09-13','2019-09-15',
+             '2019-09-17', '2019-09-19']
+    temps = [12.2, 12, 10, 10.7, 11.7, 9.5, 7.8, 8.5, 5.5, 4]
+
+    var stmt = db.prepare("DELETE FROM Weather");
+    stmt.run();
+    stmt.finalize();
+
+    var stmt = db.prepare("INSERT INTO Weather VALUES (?, ?, ?)");
+    for( var i = 0; i < dates.length; i++) {
+        stmt.run(temps[i], dates[i], location);
+    }
+    stmt.finalize();
+ 
+    stmt = db.prepare("SELECT * FROM Weather");
+    stmt.each((err, row) => {
+        console.log(row);
+    }, function(err, count) {
+        stmt.finalize();
+    });
+}); 
 
 router.get('/', (req, res) => {
 
