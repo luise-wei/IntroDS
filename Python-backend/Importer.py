adde97a1bd1aa9f9a1a667c82e1470c20277aeaa
Vincent <kuehnvini@gmail.com>
2019-09-21 23:59:06 +0300

    Importer changed
    Signed-off-by: Vincent <kuehnvini@gmail.com>

--- Python-backend/Importer.py
+++ Python-backend/Importer.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import Datapoint as dp
+#import Datapoint as dp
 import re
 
 
@@ -13,30 +13,46 @@ class Importer():
 
     def import_all(self, weather_paths, k_index_paths):
         ''' Main importer for the data '''
-        self.df = self.__import_weather('Weather\\' + weather_paths[0])
-        self.completed = self.__import_kIndex(self.df, 'GeoData\\' + k_index_paths[0])
+        self.df = self.__load_all_weather(weather_paths) #self.__import_weather('Weather\\' + weather_paths[0])
+        self.df = self.__load_all_kIndex(k_index_paths, self.df)
+        self.completed = True
         print(self.completed)
         print(self.df.head())
 
     def __import_weather(self, path):
-        df = pd.read_csv(path)
+        ''' Import data from a weather station in csv '''
+
+        df = pd.read_csv("Weather\\" + path)
         df = df.drop('Time zone', axis = 1)
         df = self.__replace(df, 'Time')
         df = df[df.Time.isin([0, 3, 6, 9, 12, 15, 18, 21])] #Time of the k Index messurement
+        #rename columns
+        path = path.split('.')[0]
 
-        return df
+        i = 0
+        indexes = {}
 
-    def __import_kIndex(self, df, path):
-        data = open(path).read().split('\n')
+        while i < len(df.columns):
+            indexes[df.columns[i]] = path + '_' + df.columns[i]
+            i += 1
+        
+        return df.rename(columns = indexes)
+
+    def __import_kIndex(self, path):
+        ''' Import  '''
+
+        data = open('GeoData\\' + path).read().split('\n')
         data.remove('')
         data = list(filter(lambda x: x[0].isdigit(), data))
 
         result = self.__parse_data(data)
-        df = self.__to_dataframe(result, df)
+        #df = self.__to_dataframe(result, df)
 
-        return True
+        return result
 
     def __replace(self, df, cat):
+        ''' replace object values with numerical '''
+
         labels = df[cat].astype('category').cat.categories.tolist()
         replace_map = {cat : {k: v for k,v in zip(labels,list(range(0,len(labels))))}}
         df.replace(replace_map, inplace = True)
@@ -44,6 +60,8 @@ class Importer():
         return df
 
     def __parse_data(self, data):
+        ''' Parses a GeoData txt file returns a list of the data '''
+
         result = []
 
         for elem in data:
@@ -60,15 +78,18 @@ class Importer():
         return result
 
     def __to_dataframe(self, data, df):
-        k1 = ['']
-        k2 = ['']
-        k3 = ['']
+        ''' Merges geo and weather data '''
+
+        k1 = []
+        k2 = []
+        k3 = []
 
         for elem in data:
              k1.extend(elem[3])
              k2.extend(elem[4])
              k3.extend(elem[5])
 
+        print(df)
         print(df.shape)
         print(len(k1))
         df['Fredericksburg'] = k1
@@ -77,5 +98,28 @@ class Importer():
 
         return df
 
+    def __load_all_weather(self, path_list):
+        ''' Loads multiple weather files '''
+
+        frames = []
+
+        for path in path_list:
+            frames.append(self.__import_weather(path))
+
+        df = pd.concat(frames, axis=1, sort=True)
+        return df
+
+    def __load_all_kIndex(self, path_list, df):
+        ''' Loads multiple GeoData files '''
+
+        results = []
+
+        for path in path_list:
+            results.extend(self.__import_kIndex(path))
+
+        return self.__to_dataframe(results, df)
+
+
 x = Importer()
-x.import_all(['test.csv'], ['2018_DGD.txt'])
+x.import_all(['test1.csv'], ['2017_DGD.txt', '2018_DGD.txt'])
+#print(x.df.Time)
